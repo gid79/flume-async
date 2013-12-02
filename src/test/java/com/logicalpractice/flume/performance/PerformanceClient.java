f9450f36c6e352894715bdc91d8dc33cce66e000
Gareth Davis <gareth.davis@mac.com>
2013-12-02 21:25:46 +0000

    fiddling with the performance test client

--- src/test/java/com/logicalpractice/flume/performance/PerformanceClient.java
+++ src/test/java/com/logicalpractice/flume/performance/PerformanceClient.java
@@ -1,7 +1,11 @@
-package com.logicalpractice.flume;
+package com.logicalpractice.flume.performance;
 
-import ch.qos.logback.classic.BasicConfigurator;
 import ch.qos.logback.classic.LoggerContext;
+import com.codahale.metrics.Histogram;
+import com.codahale.metrics.Meter;
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.Snapshot;
+import com.google.common.collect.Lists;
 import com.logicalpractice.flume.api.NettyLoadBalancingRpcClient;
 import net.sourceforge.argparse4j.ArgumentParsers;
 import net.sourceforge.argparse4j.inf.ArgumentParser;
@@ -11,6 +15,7 @@ import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.Level;
 import org.apache.flume.Event;
 import org.apache.flume.EventDeliveryException;
+import org.apache.flume.FlumeException;
 import org.apache.flume.api.RpcClient;
 import org.apache.flume.api.RpcClientConfigurationConstants;
 import org.apache.flume.api.RpcClientFactory;
@@ -56,25 +61,45 @@ public class PerformanceClient {
         ns.getInt("batch_size"),
         ns.getInt("message_size")
     );
-    for( int client = 0; client< clients ; client ++ ) {
-      Properties props = new Properties();
-      props.setProperty(RpcClientConfigurationConstants.CONFIG_CLIENT_TYPE, NettyLoadBalancingRpcClient.class.getName());
-      props.setProperty(RpcClientConfigurationConstants.CONFIG_HOSTS, "h1");
-      props.setProperty(RpcClientConfigurationConstants.CONFIG_HOSTS_PREFIX + ".h1", "localhost:2001");
-      RpcClient rpcClient = RpcClientFactory.getInstance(props);
+    MetricRegistry registry = new MetricRegistry();
+    ClientMetrics metrics = new ClientMetrics(registry);
+    List<RpcClient> allClients = Lists.newLinkedList();
+    try{
+      for( int client = 0; client < clients ; client ++ ) {
+        Properties props = new Properties();
+        props.setProperty(RpcClientConfigurationConstants.CONFIG_CLIENT_TYPE, NettyLoadBalancingRpcClient.class.getName());
+        props.setProperty(RpcClientConfigurationConstants.CONFIG_HOSTS, "h1");
+        props.setProperty(RpcClientConfigurationConstants.CONFIG_HOSTS_PREFIX + "h1", "localhost:2001");
+        RpcClient rpcClient = RpcClientFactory.getInstance(props);
 
-      for( int thread = 0; thread < threads; thread ++ ) {
-        executor.submit(new ContinuousRun(latch, rpcClient, configuration));
+        for( int thread = 0; thread < threads; thread ++ ) {
+          executor.submit(new ContinuousRun(latch, rpcClient, metrics, configuration));
+        }
+        allClients.add(rpcClient);
+      }
+
+      logger.info("Beginning the run: clients:" + clients +
+          ", threads:" + threads +
+          ", iterations:" + ns.getInt("iterations") +
+          ", batchSize:" + ns.getInt("batch_size") +
+          ", messageSize:" + ns.getInt("message_size"));
+
+      latch.countDown();
+      latch.await();
+
+      executor.shutdown();
+      executor.awaitTermination(24, TimeUnit.HOURS);
+
+      logger.info("Test has finished :{}", metrics);
+    } finally {
+      for (RpcClient client : allClients) {
+        try {
+          client.close();
+        } catch (FlumeException e) {
+          // ignore
+        }
       }
     }
-
-    logger.info("Beginning the run...");
-    latch.countDown();
-    latch.await();
-
-    executor.shutdown();
-    executor.awaitTermination(24, TimeUnit.HOURS);
-    logger.info("Test has finished");
   }
 
   private static void configureLogging(Namespace ns) {
@@ -86,7 +111,6 @@ public class PerformanceClient {
       root.setLevel(Level.ERROR);
       lc.getLogger(PerformanceClient.class).setLevel(Level.INFO);
     }
-    BasicConfigurator.configure(lc);
   }
 
   private static ArgumentParser buildArgumentParser() {
@@ -144,15 +168,50 @@ public class PerformanceClient {
     }
   }
 
+  static class ClientMetrics {
+
+    private final Histogram appendBatch;
+    private final Meter errorRate;
+
+    ClientMetrics(MetricRegistry registry) {
+      appendBatch = registry.histogram("appendBatch");
+      errorRate = registry.meter("errors");
+    }
+
+    public void markAppendBatch(long timeNs) { appendBatch.update(timeNs); }
+
+    public void markError(){ errorRate.mark(); }
+
+    @Override
+    public String toString() {
+      Snapshot ss = appendBatch.getSnapshot();
+      return "Response times: " +
+          String.format("50th:%5.3fms, 75th:%5.3fms, 95th:%5.3fms, 99th:%5.3fms",
+              millis(ss.getMean()),
+              millis(ss.get75thPercentile()),
+              millis(ss.get95thPercentile()),
+              millis(ss.get99thPercentile())) +
+          ", Errors: " + errorRate.getCount() + " at "
+          + String.format("%.1f", errorRate.getOneMinuteRate()) + "/s" ;
+
+    }
+    public double millis(double nanos) {
+      return nanos / 1000000.0;
+    }
+
+  }
+
   static abstract class ClientRunnable implements Runnable {
     private Logger logger = (Logger) LoggerFactory.getLogger(ClientRunnable.class);
     private final CountDownLatch latch;
     private final RpcClient client;
+    private final ClientMetrics metrics;
     private final TaskConfiguration configuration;
 
-    ClientRunnable(CountDownLatch latch, RpcClient client, TaskConfiguration configuration) {
+    ClientRunnable(CountDownLatch latch, RpcClient client, ClientMetrics metrics, TaskConfiguration configuration) {
       this.latch = latch;
       this.client = client;
+      this.metrics = metrics;
       this.configuration = configuration;
     }
 
@@ -186,17 +245,20 @@ public class PerformanceClient {
         events.add(EventBuilder.withBody(("event: " + i).getBytes()));
       }
 
+      long start = System.nanoTime();
       try {
         client.appendBatch(events);
       } catch (EventDeliveryException e) {
-//        markError(e);
+        metrics.markError();
+      } finally {
+        metrics.markAppendBatch(System.nanoTime() - start);
       }
     }
   }
 
   static class ContinuousRun extends ClientRunnable {
-    ContinuousRun(CountDownLatch latch, RpcClient client, TaskConfiguration configuration) {
-      super(latch, client, configuration);
+    ContinuousRun(CountDownLatch latch, RpcClient client, ClientMetrics metrics, TaskConfiguration configuration) {
+      super(latch, client, metrics, configuration);
     }
 
     @Override
--- src/test/java/com/logicalpractice/flume/performance/RemoteServer.java
+++ src/test/java/com/logicalpractice/flume/performance/RemoteServer.java
@@ -1,17 +1,25 @@
-package com.logicalpractice.flume;
+package com.logicalpractice.flume.performance;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
 import com.logicalpractice.flume.api.RpcTestUtils;
 import net.sourceforge.argparse4j.ArgumentParsers;
 import net.sourceforge.argparse4j.inf.ArgumentParser;
+import net.sourceforge.argparse4j.inf.ArgumentParserException;
+import net.sourceforge.argparse4j.inf.Namespace;
 import org.apache.avro.AvroRemoteException;
 import org.apache.commons.math3.distribution.NormalDistribution;
 import org.apache.commons.math3.distribution.RealDistribution;
 import org.apache.flume.source.avro.AvroFlumeEvent;
 import org.apache.flume.source.avro.AvroSourceProtocol;
 import org.apache.flume.source.avro.Status;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+
+import static net.sourceforge.argparse4j.impl.Arguments.storeTrue;
 
 /**
  *
@@ -19,12 +27,49 @@ import java.util.concurrent.TimeUnit;
 public class RemoteServer {
 
   public static void main(String[] args) {
-    ArgumentParser parser = ArgumentParsers.newArgumentParser("RemoteServer");
+    ArgumentParser parser = buildArgumentParser();
 
-    Handler h = new Handler(100, TimeUnit.MILLISECONDS, new NormalDistribution(100, 10));
+    Namespace ns = null;
+    try {
+        ns = parser.parseArgs(args);
+    } catch (ArgumentParserException e) {
+        parser.handleError(e);
+        System.exit(1);
+    }
+    configureLogging(ns);
+    Handler h = new Handler(
+        ns.getInt("delay"),
+        TimeUnit.MILLISECONDS,
+        null
+    );
     RpcTestUtils.startServer(h, 2001);
   }
 
+  private static ArgumentParser buildArgumentParser() {
+    ArgumentParser parser = ArgumentParsers.newArgumentParser("RemoteServer");
+
+    parser.addArgument("-v","--verbose")
+        .action(storeTrue())
+        .setDefault(false)
+    ;
+
+    parser.addArgument("-d","--delay")
+        .type(Integer.class)
+        .setDefault(-1);
+
+    return parser;
+  }
+
+  private static void configureLogging(Namespace ns) {
+    LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+    Logger root = lc.getLogger(Logger.ROOT_LOGGER_NAME);
+    if( ns.getBoolean("verbose") ){
+      root.setLevel(Level.DEBUG);
+    } else {
+      root.setLevel(Level.ERROR);
+      lc.getLogger(RemoteServer.class).setLevel(Level.INFO);
+    }
+  }
   static class Handler implements AvroSourceProtocol {
     private final long delayNS ;
     private final RealDistribution distribution;
